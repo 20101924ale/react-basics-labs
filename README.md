32796312d95ca73abb9117c94918c4d8b8fcb556
20101924ale <114234258+20101924ale@users.noreply.github.com>
2024-09-20 17:07:12 +0100

    completed tasky lab 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-basics-labs
+
+Labs for Web App Development Week 1, 2 and 3
--- task/src/App.js
+++ task/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Tasky</h1>
+      <Task title="Dishes" deadline="Today">
+      wash dishes, load the dishwasher
+      </Task>
+      <Task title="Laundry" deadline="Tomorrow" >
+        do some laundry, fold some clothes </Task>
+      <Task title="Tidy" deadline="Today">
+        tidy up the mess </Task>
     </div>
   );
 }
